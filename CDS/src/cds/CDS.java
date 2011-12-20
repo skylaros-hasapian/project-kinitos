7dba9bcf3fd5659e8468fd97f6e52d3ab078352b
George Hatzinotas <metalastor@gmail.com>
2011-12-20 19:51:04 +0200

    hello world

--- CDS/src/cds/CDS.java
+++ CDS/src/cds/CDS.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package cds;
+package cds; 
 
 /**
  *
@@ -15,5 +15,6 @@ public class CDS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World!");
     }
 }
