5cd8412cc349afa22437d84b858d9c98cb1eb342
George Hatzinotas <metalastor@gmail.com>
2011-12-20 19:54:03 +0200

    mac hello Moon

--- CDS/src/cds/CDS.java
+++ CDS/src/cds/CDS.java
@@ -16,5 +16,6 @@ public class CDS {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
+        System.out.println("Hello Moon");
     }
 }
