b53f1fd028bed1cabbfb41e1bc033a356a01ac74
George Hatzinotas <metalastor@gmail.com>
2011-12-20 18:27:44 +0200

    Java project created. try to open in with netbeans

---
+++ CDS/src/cds/CDS.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cds;
+
+/**
+ *
+ * @author georgehatzinotas
+ */
+public class CDS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
