deff2b0bf25189690029d404c9a5537b51535bf1
George Hatzinotas <metalastor@gmail.com>
2011-12-26 18:05:29 +0200

    started writing the code

--- CDS/src/cds/CDS.java
+++ CDS/src/cds/CDS.java
@@ -20,9 +20,8 @@ public class CDS {
         {false, false, false, false, false, true, false, true, false},
         {false, false, false, false, false, false, true, false, true},
         {false, false, false, false, false, false, false, false, false}
-    };
-    
-    public static int degree[] = {0,1,1,2,2,2,3,4,5};
+    }; //αν το graph[i][i]==true τότε το κόμβος με id=i είναι η ρίζα του δέντρου
+    public static int degree[] = {0, 1, 1, 2, 2, 2, 3, 4, 5};
 
     /**
      * @param args the command line arguments
@@ -43,13 +42,32 @@ public class CDS {
         Node[] komvos = new Node[n];
 
         for (i = 0; i < n; i++) {
-            komvos[i] = new Node(i,degree[i]);
+            komvos[i] = new Node(i, degree[i]);
         }
-        
+
         System.out.println("ID\tDegree");
 
         for (i = 0; i < n; i++) {
-            System.out.println(komvos[i].id+1+"\t"+komvos[i].degree);
+            System.out.println(komvos[i].id + 1 + "\t" + komvos[i].degree);
         }
+
+        for (i = 0; (i < 9 && !graph[i][i]); i++);
+        komvos[i].color = "Black";
+        CNM(komvos[i].color, komvos[i].degree, n, komvos);
+
+        System.out.println(komvos[1].color);
+
+    }
+
+    static void CNM(String color, int j, int n, Node komvos[]) {
+        if (color.equals("Black")) {
+            for (int k = 0; k < n; k++) {
+                if (komvos[k].degree == komvos[j].degree + 1) {
+                    komvos[k].color = "White";
+                }
+
+            }
+        }
+        //throw new UnsupportedOperationException("Not yet implemented");
     }
 }
--- CDS/src/cds/Node.java
+++ CDS/src/cds/Node.java
@@ -12,9 +12,14 @@ public class Node {
 
     int id;
     int degree;
+    String color;
+    boolean outstanding;
 
     public Node(int id, int degree) {
         this.id = id;
         this.degree = degree;
+        color = null;
+        outstanding = false;
+
     }
 }
